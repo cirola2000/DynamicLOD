a26a9428c8533dfae3cd35053c5ebf476003da75
Cirola2000 <cirobaroneto@gmail.com>
2015-03-03 13:39:31 +0100

    checking error using rapper

--- src/main/java/dataid/files/RunCommand.java
+++ src/main/java/dataid/files/RunCommand.java
@@ -11,9 +11,9 @@ import dataid.DataIDGeneralProperties;
 import dataid.exceptions.DataIDException;
 
 public class RunCommand {
-	
-	public ArrayList<String> runRapper(String c) throws Exception{
-		
+
+	public ArrayList<String> runRapper(String c) throws Exception {
+
 		ArrayList<String> results = new ArrayList<String>();
 		String[] cmd = { "/bin/sh", "-c", c };
 
@@ -33,76 +33,41 @@ public class RunCommand {
 		String s = null;
 		while ((s = stdInput.readLine()) != null) {
 			results.add(s);
-			System.out.println("Awk output: "+s);
+			System.out.println("Awk output: " + s);
 		}
 
 		// read any errors from the attempted command
+		// dont show more than 100 errors.
+		int errorCount = 0;
 		while ((s = stdError.readLine()) != null) {
-			DataID.bean.addDisplayMessage(
-					DataIDGeneralProperties.MESSAGE_ERROR, s);
-			if(c.contains("rapper")){
-				if(s.contains("returned")){
-					String a[] =s.split(" "); 
-					results.add("[totalTriples] "+Integer.parseInt(a[3]));
+			if (errorCount < 100) {
+				errorCount++;
+				DataID.bean.addDisplayMessage(
+						DataIDGeneralProperties.MESSAGE_ERROR, s);
+				if (c.contains("rapper")) {
+					if (s.contains("returned")) {
+						String a[] = s.split(" ");
+						results.add("[totalTriples] " + Integer.parseInt(a[3]));
+					}
 				}
+				System.out.println("Rapper output: " + s);
 			}
-			System.out.println("Rapper output: "+s);
+			else{
+				proc.destroy();
+				throw new DataIDException("Too many errors while parsing.");
+			}
 		}
 
 		proc.waitFor();
 		if (proc.exitValue() != 0)
-			throw new DataIDException("Something went wrong while running AWK. Check LOG file.");
+			throw new DataIDException(
+					"Something went wrong while running AWK. Check LOG file.");
 
 		stdInput.close();
 		stdError.close();
 		proc.destroy();
-		
+
 		return results;
 	}
-	
-//	public static int run(String c) throws Exception {
-//
-//		String[] cmd = { "/bin/sh", "-c", c };
-//
-//		DataID.bean.addDisplayMessage(DataIDGeneralProperties.MESSAGE_LOG,
-//				"<b>Running command:</b> <i>" + c + "</i>");
-//
-//		Runtime rt = Runtime.getRuntime();
-//		Process proc = rt.exec(cmd);
-//
-//		BufferedReader stdInput = new BufferedReader(new InputStreamReader(
-//				proc.getInputStream()));
-//
-//		BufferedReader stdError = new BufferedReader(new InputStreamReader(
-//				proc.getErrorStream()));
-//
-//		// read the output from the command
-//		String s = null;
-//		while ((s = stdInput.readLine()) != null) {
-//			DataID.bean.addDisplayMessage(DataIDGeneralProperties.MESSAGE_LOG,
-//					s);
-//		}
-//
-//		// read any errors from the attempted command
-//		while ((s = stdError.readLine()) != null) {
-//			DataID.bean.addDisplayMessage(
-//					DataIDGeneralProperties.MESSAGE_ERROR, s);
-//			if(c.contains("rapper")){
-//				if(s.contains("returned")){
-//					String a[] =s.split(" "); 
-//					return Integer.parseInt(a[3]);
-//				}
-//			}
-//		}
-//
-//		proc.waitFor();
-//		if (proc.exitValue() != 0)
-//			throw new DataIDException("Something went wrong. Check LOG file.");
-//
-//		stdInput.close();
-//		stdError.close();
-//		proc.destroy();
-//		
-//		return 0;
-//	}
+
 }
