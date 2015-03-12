aa9fd02794d951aa146918db123b08283b2a854a
Cirola2000 <cirobaroneto@gmail.com>
2015-03-12 11:13:11 +0100

    added CreateOutputByDistributions.java class

---
+++ src/main/java/dataid/server/CreateOutputByDistributions.java
@@ -0,0 +1,114 @@
+package dataid.server;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+
+import dataid.mongodb.actions.Queries;
+import dataid.mongodb.objects.DatasetMongoDBObject;
+import dataid.mongodb.objects.DistributionMongoDBObject;
+import dataid.mongodb.objects.LinksetMongoDBObject;
+import dataid.ontology.Dataset;
+import dataid.ontology.NS;
+
+public class CreateOutputByDistributions extends HttpServlet {
+
+	private Model outModel =null;
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		printOutput(response);
+	}
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		printOutput(response);
+	}
+	
+	public void printOutput(HttpServletResponse response){
+
+		try {
+			outModel = ModelFactory.createDefaultModel();
+
+			outModel.setNsPrefix("rdfs", NS.RDFS_URI);
+			outModel.setNsPrefix("dcat", NS.DCAT_URI);
+			outModel.setNsPrefix("void", NS.VOID_URI);
+			outModel.setNsPrefix("sd", NS.SD_URI);
+			outModel.setNsPrefix("prov", NS.PROV_URI);
+			outModel.setNsPrefix("dct", NS.DCT_URI);
+			outModel.setNsPrefix("xsd", NS.XSD_URI);
+			outModel.setNsPrefix("foaf", NS.FOAF_URI);
+			outModel.setNsPrefix("dataid", NS.DATAID_URI);
+
+			// get all datasets
+			Queries queries = new Queries();
+
+			ArrayList<DistributionMongoDBObject> distributionList = queries.getDistributions();
+
+			if (distributionList != null)
+				for (DistributionMongoDBObject distribution : distributionList) {
+					Resource r = outModel.createResource(distribution.getUri());
+					r.addProperty(
+							Dataset.dataIDType,
+							ResourceFactory.createResource(NS.VOID_URI
+									+ "Dataset"));
+					r.addProperty(
+							Dataset.title,
+							distribution.getTitle());
+					r.addProperty(
+							Dataset.label,
+							distribution.getTitle());
+				}
+
+			ArrayList<LinksetMongoDBObject> linksetList = queries
+					.getLinksetsGroupByDistributions();
+
+			if (linksetList != null)
+				for (LinksetMongoDBObject linkset : linksetList) {
+//					if(linkset.getLinks()>30)
+					System.out.println("oi "+linkset.getUri());
+						Resource r = outModel.createResource(linkset.getUri());
+						r.addProperty(
+								Dataset.dataIDType,
+								ResourceFactory.createResource(NS.VOID_URI
+										+ "Linkset"));
+						r.addProperty(
+								ResourceFactory.createProperty(NS.VOID_URI
+										+ "subjectsTarget"), ResourceFactory
+										.createProperty(linkset
+												.getSubjectsDistributionTarget()
+												.toString()));
+						r.addProperty(ResourceFactory
+								.createProperty(NS.VOID_URI + "objectsTarget"),
+								ResourceFactory.createProperty(linkset
+										.getObjectsDistributionTarget().toString()));
+						r.addProperty(ResourceFactory
+								.createProperty(NS.VOID_URI + "triples"),
+								ResourceFactory.createPlainLiteral(String.valueOf(linkset.getLinks())));
+					}
+				
+
+			if (linksetList.isEmpty() && distributionList.isEmpty())
+				response.getWriter()
+						.println(
+								"There are no DataIDs files inserted! Please insert a DataID file and try again.");
+			else {
+
+				outModel.write(System.out, "TURTLE");
+				outModel.write(response.getWriter(), "TURTLE");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
