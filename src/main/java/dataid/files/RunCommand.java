102467fdaf0ee4d6f11743fc464ca149372eb256
Cirola2000 <cirobaroneto@gmail.com>
2015-04-30 19:27:33 +0200

    -

--- src/main/java/dataid/files/RunCommand.java
+++ src/main/java/dataid/files/RunCommand.java
@@ -1,21 +1,11 @@
 package dataid.files;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Queue;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.log4j.Logger;
-import org.junit.Test;
-
-import dataid.Manager;
-import dataid.DataIDGeneralProperties;
-import dataid.exceptions.DataIDException;
-import dataid.server.DataIDBean;
 
 public class RunCommand {
 	
@@ -43,7 +33,7 @@ public class RunCommand {
 
 		// read the output from the command
 		String string = null;
-		while ((string = stdInput.readLine()) != null) {
+		while ((string = fixEncoding(stdInput.readLine())) != null) {
 
 			if (string.contains("[totalTriples]")) {
 				String a[] = string.split(" ");
@@ -126,5 +116,58 @@ public class RunCommand {
 		System.out.println("Process closed");
 
 	}
+	
+	
+	 public static String fixEncoding(String latin1) {
+		  try {
+		   byte[] bytes = latin1.getBytes("ISO-8859-1");
+		   if (!validUTF8(bytes))
+		    return latin1;   
+		   return new String(bytes, "UTF-8");  
+		  } catch (UnsupportedEncodingException e) {
+		   // Impossible, throw unchecked
+		   throw new IllegalStateException("No Latin1 or UTF-8: " + e.getMessage());
+		  }
+
+		 }
+
+		 public static boolean validUTF8(byte[] input) {
+		  int i = 0;
+		  // Check for BOM
+		  if (input.length >= 3 && (input[0] & 0xFF) == 0xEF
+		    && (input[1] & 0xFF) == 0xBB & (input[2] & 0xFF) == 0xBF) {
+		   i = 3;
+		  }
+
+		  int end;
+		  for (int j = input.length; i < j; ++i) {
+		   int octet = input[i];
+		   if ((octet & 0x80) == 0) {
+		    continue; // ASCII
+		   }
+
+		   // Check for UTF-8 leading byte
+		   if ((octet & 0xE0) == 0xC0) {
+		    end = i + 1;
+		   } else if ((octet & 0xF0) == 0xE0) {
+		    end = i + 2;
+		   } else if ((octet & 0xF8) == 0xF0) {
+		    end = i + 3;
+		   } else {
+		    // Java only supports BMP so 3 is max
+		    return false;
+		   }
+
+		   while (i < end) {
+		    i++;
+		    octet = input[i];
+		    if ((octet & 0xC0) != 0x80) {
+		     // Not a valid trailing byte
+		     return false;
+		    }
+		   }
+		  }
+		  return true;
+		 }
 
 }
